a7f260925c2c1646f2f0f60a018a0d2fec468756
teejay327 <mrenergy@hotmail.com>
2024-08-19 23:10:00 +1000

    Changed Player component jsx to conditionally render the name of the player

---
+++ src/components/Player.jsx
@@ -0,0 +1,24 @@
+import {useState} from 'react';
+
+const Player = ({name, symbol}) => {
+  const [ isEditing, setIsEditing ] = useState(false);
+
+  function handleEditButton() {
+    setIsEditing(true);
+  }
+
+  let playerName = <span className="player-name">{ name }</span>;
+  if (isEditing) playerName = <input type="text" required/>
+
+    return (
+    <li>
+      <span className="player">
+        { playerName }
+        <span className="player-symbol">{ symbol }</span>
+      </span>
+      <button onClick={handleEditButton}>Edit</button>
+    </li>
+  );
+}
+
+export default Player;
